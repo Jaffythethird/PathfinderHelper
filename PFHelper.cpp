2d42d5578c5348ba229df534168b295dd9b4de24
Jordan Fry <jaffythethird@gmail.com>
2020-04-25 15:42:46 -0400

    Started working on PFHelper

--- PFHelper.cpp
+++ PFHelper.cpp
@@ -2,8 +2,34 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
-
-int main(int argc, const char* argv[])
+using namespace std;
+int main()
 {
-	
+	bool running = true;
+	String input;
+	String[] parsed;
+	while(running)
+	{
+		cin >> input;
+		parsed = cmdParser(input);
+		switch (parsed[0]) {
+			case "exit":
+				running = false;
+				break;
+			case "help":
+
+				break;
+			default:
+				cout << "Unrecognized command. Type \"Help\" for assistance.";
+				break;
+
+		}
+		
+	}
+}
+
+String[] cmdParser(String args)
+{
+	// Temporary Fix
+	return {args};
 }
